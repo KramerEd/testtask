9d8b0e65aa19ca95e5804b8dba58335b703e50ed
eduard.kramarenko <eduard.kramarenko@theopentag.com>
2024-11-21 08:26:27 +0200

    init navigation

---
+++ src/screens/Home/HomeScreen/index.tsx
@@ -0,0 +1,10 @@
+import {SafeAreaView, Text} from 'react-native';
+
+export const HomeScreen = () => {
+  return (
+    <SafeAreaView
+      style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
